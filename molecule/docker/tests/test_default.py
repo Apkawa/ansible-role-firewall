51520f80209b2f3397f1a5bfeb3e54cfc15fff2b
Arkadii Ivanov <apkawa@gmail.com>
2019-09-16 18:38:51 +0300

    fix access host port from container

--- molecule/docker/tests/test_default.py
+++ molecule/docker/tests/test_default.py
@@ -14,6 +14,7 @@ testinfra_hosts = RUNNER.get_hosts('all')
 def test_check_open(host):
     host.run('docker rm -f test')
     assert host.run('docker run -p 80:80 -d --name test nginxdemos/hello:plain-text').rc == 0
+    assert host.run('docker run --rm --net=host busybox nc -zvw 1 127.0.0.1 80').rc == 0
     host.run('docker rm -f test')
     # TODO extend verify
 
@@ -28,3 +29,11 @@ def test_rules(host):
 def test_docker(host):
     cmd = host.run('docker run --rm busybox ping -c 1 -q 8.8.8.8')
     assert cmd.rc == 0, cmd.stdout
+
+
+def test_access_from_docker_to_host_port(host):
+    host.run('docker rm -f echo echo_http')
+    assert host.run(
+        'docker run --rm -e TCP_PORT=2701 -p 2777:2701 -d --name echo cjimti/go-echo').rc == 0
+    assert host.run('docker run --rm busybox nc -zvw 1 172.18.0.1 2777').rc == 0
+    host.run('docker rm -f echo echo_http')
