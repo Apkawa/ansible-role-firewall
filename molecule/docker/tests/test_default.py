70d4c02e37959646dc18b0707760c1dc2f7453f9
Arkadii Ivanov <apkawa@gmail.com>
2019-09-16 15:56:24 +0300

    fix store docker nat rules

--- molecule/docker/tests/test_default.py
+++ molecule/docker/tests/test_default.py
@@ -10,14 +10,21 @@ RUNNER = testinfra.utils.ansible_runner.AnsibleRunner(
 
 testinfra_hosts = RUNNER.get_hosts('all')
 
+
 def test_check_open(host):
     host.run('docker rm -f test')
     assert host.run('docker run -p 80:80 -d --name test nginxdemos/hello:plain-text').rc == 0
     host.run('docker rm -f test')
     # TODO extend verify
 
+
 def test_rules(host):
     cmd = host.run('iptables -L -n')
     assert cmd.rc == 0
     # TODO improve checks
     assert 'DOCKER' in cmd.stdout.strip()
+
+
+def test_docker(host):
+    cmd = host.run('docker run --rm busybox ping -c 1 -q 8.8.8.8')
+    assert cmd.rc == 0, cmd.stdout
