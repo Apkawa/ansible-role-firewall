51ba4dad1cc3eb282912f3155377487759699fd0
apkawa <apkawa@gmail.com>
2019-10-29 13:57:30 +0300

    refactoring and fixes for docker

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -14,6 +14,8 @@ def test_rules(host):
 Chain INPUT (policy ACCEPT)
 target     prot opt source               destination         
 ACCEPT     all  --  0.0.0.0/0            0.0.0.0/0           
+ACCEPT     icmp --  0.0.0.0/0            0.0.0.0/0           
+ACCEPT     udp  --  0.0.0.0/0            0.0.0.0/0            udp spt:123
 ACCEPT     tcp  --  0.0.0.0/0            0.0.0.0/0            tcp dpt:9123
 ACCEPT     tcp  --  8.8.8.8              0.0.0.0/0            tcp dpt:1042
 DROP       tcp  --  0.0.0.0/0            0.0.0.0/0            tcp dpt:1042
@@ -30,8 +32,6 @@ ACCEPT     udp  --  10.0.0.0/24          0.0.0.0/0            udp dpt:1043
 DROP       udp  --  10.0.0.0/24          0.0.0.0/0            udp dpt:1043
 ACCEPT     udp  --  10.0.0.1             0.0.0.0/0            udp dpt:1044
 DROP       udp  --  10.0.0.0/24          0.0.0.0/0            udp dpt:1044
-ACCEPT     icmp --  0.0.0.0/0            0.0.0.0/0           
-ACCEPT     udp  --  0.0.0.0/0            0.0.0.0/0            udp spt:123
 ACCEPT     all  --  0.0.0.0/0            0.0.0.0/0            state RELATED,ESTABLISHED
 LOG        all  --  0.0.0.0/0            0.0.0.0/0            limit: avg 15/min burst 5 LOG flags 0 level 7 prefix "Dropped by firewall: "
 DROP       all  --  0.0.0.0/0            0.0.0.0/0           
@@ -42,4 +42,10 @@ target     prot opt source               destination
 Chain OUTPUT (policy ACCEPT)
 target     prot opt source               destination         
 ACCEPT     udp  --  0.0.0.0/0            0.0.0.0/0            udp dpt:123
+
+Chain DOCKER-USER (0 references)
+target     prot opt source               destination         
+
+Chain FILTERS (0 references)
+target     prot opt source               destination   
 """.strip()
